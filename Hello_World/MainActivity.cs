36b7dd820c087480d82f1318d6089d0f945a3ccd
Riley Moses <victi.resurgo@gmail.com>
2014-04-14 16:06:38 -0600

    Three ways to make the same network request

--- Hello_World/MainActivity.cs
+++ Hello_World/MainActivity.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text;
 using Android.App;
 using Android.Content;
 using Android.Runtime;
@@ -12,7 +17,8 @@ namespace Hello_World
 	[Activity (Label = "Hello World", MainLauncher = true)]
 	public class MainActivity : Activity
 	{
-		int count = 1;
+		private int count = 1;
+		private Toast t;
 
 		protected override void OnCreate(Bundle bundle)
 		{
@@ -39,9 +45,67 @@ namespace Hello_World
 				StartActivity(secondIntent);
 			};
 
+			/*
+			 * Multiple ways to send GET request
+			 */
+
+
+			String json = "{\"key\":\"value\"}";
+			var url = new Uri(String.Format("http://validate.jsontest.com/?json={0}", json));
+
+			// Method 1 - WebClient
 			networkRequestButton.Click += delegate {
-				Toast.MakeText(this, Resource.String.network_request, ToastLength.Short).Show();
+				var webClient = new WebClient ();
+				webClient.DownloadStringCompleted += (s, e) => {
+					var text = e.Result;
+					PrintResult(1, text);
+				};
+				webClient.DownloadStringAsync (url);
 			};
+
+			// Method 2 - WebRequest
+			networkRequestButton.Click += delegate {
+				var request = WebRequest.Create (url);
+				request.BeginGetResponse(Method2, request);
+			};
+
+			// Method 3 - HttpClient
+			networkRequestButton.Click += async delegate {
+				Task<String> contentsTask = Method3(url);
+				String result = await contentsTask;
+				PrintResult(3, result);
+			};
+		}
+
+		// Used with "Method 2" above
+		private void Method2(IAsyncResult result)
+		{
+			var request = result.AsyncState as HttpWebRequest;
+			try
+			{
+				var response = request.EndGetResponse(result);
+				var reader = new System.IO.StreamReader(response.GetResponseStream());
+				PrintResult(2, reader.ReadToEnd());
+			}
+			catch (Exception e) { }
+		}
+
+		// Used with "Method 3" above
+		private async Task<String> Method3(Uri url)
+		{
+			var httpClient = new HttpClient();
+			Task<String> contentsTask = httpClient.GetStringAsync(url);
+			// "await" returns control to the caller and the task continues to run on another thread
+			String contents = await contentsTask;
+			return contents;
+		}
+
+		private void PrintResult(int methodNum, String result)
+		{
+			Console.WriteLine("Method " + methodNum);
+			Console.WriteLine(result);
+//			t = Toast.MakeText(this, Resources.GetString(Resource.String.network_request) + " " + methodNum, ToastLength.Short);
+//			t.Show();
 		}
 	}
 }
